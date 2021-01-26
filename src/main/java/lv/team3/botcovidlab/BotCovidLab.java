8be13ed5bb6add23ab8e1a6e075f1ecc1e05bd4c
JanisValentinovics <valentinovicskristians@gmail.com>
2021-01-26 14:43:59 +0200

    Update to the sturcture

---
+++ src/main/java/lv/team3/botcovidlab/BotCovidLab.java
@@ -0,0 +1,16 @@
+package lv.team3.botcovidlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/*
+ * Entry point of SpringBoot application
+ * Janis Valentinovics
+ */
+@SpringBootApplication
+public class BotCovidLab {
+    public static void main(String[] args) {
+        SpringApplication.run(BotCovidLab.class, args);
+    }
+}
