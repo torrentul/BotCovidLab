25b329f83f8292f0491335d25b8d83e6a00e2d6f
JanisValentinovics <valentinovicskristians@gmail.com>
2021-02-03 17:57:30 +0200

    Setting up project Firebase settings for Heroku deployment.

--- src/main/java/lv/team3/botcovidlab/entityManager/FirebaseInitializer.java
+++ src/main/java/lv/team3/botcovidlab/entityManager/FirebaseInitializer.java
@@ -3,10 +3,12 @@ package lv.team3.botcovidlab.entityManager;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import lv.team3.botcovidlab.utils.HerokuUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
+import java.io.InputStream;
 
 
 /**
@@ -28,8 +30,18 @@ public class FirebaseInitializer {
                     .build();
 
             FirebaseApp.initializeApp(options);
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception ignore) {
+            try {
+                InputStream serviceAccount = HerokuUtils.getFirebaseSettings();
+                FirebaseOptions options = new FirebaseOptions.Builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .setDatabaseUrl("https://testbase-70cdb.firebaseio.com")
+                        .build();
+
+                FirebaseApp.initializeApp(options);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
     }
